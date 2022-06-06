fdd23e60bad0dedf9d218d96ea1551db8e08f204
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-06 14:53:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$testo = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!lorem"
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <h1>Censura il testo</h1>
+    <p> <?php echo $testo ?></p>
+</body>
+</html>
