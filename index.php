f8211d7db868919e67684eca13e6fb60571384b6
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-06 15:01:27 +0200

    finish

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-$testo = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!lorem"
+$testo = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!loremLorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, provident corporis nulla labore id excepturi accusamus impedit exercitationem illo quis enim voluptatum sint dicta voluptatem pariatur eos assumenda eum aliquam!lorem";
 
-
+// var_dump($_GET);
+$keyWord = $_GET["parola"];
+$filtered = str_replace($keyWord, "***", $testo);
 
 ?>
 
@@ -13,8 +15,9 @@ $testo = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
 </head>
-<body>
+<body style="text-align: center;">
     <h1>Censura il testo</h1>
-    <p> <?php echo $testo ?></p>
+    <h2>Aggiungi alla fine di questo link la parola da censurare -> http://localhost/php-badwords/?parola=</h2>
+    <p> <?php echo $filtered ?></p>
 </body>
 </html>
